f5717b01035edcc9ef6fcbadd04388fd569da24e
fkaufman-asym <fkaufman@asymmetrik.com>
2021-08-13 13:32:25 -0400

    removed boolean-query2

--- cda_client/api/query_api.py
+++ cda_client/api/query_api.py
@@ -59,6 +59,7 @@ class QueryApi(object):
 
             Keyword Args:
                 dry_run (bool): If true, don't run the query, only generate and return it.. [optional] if omitted the server will use the default value of False
+                table (str): table name. [optional] if omitted the server will use the default value of "gdc-bq-sample.cda_mvp"
                 _return_http_data_only (bool): response data without head status
                     code and headers. Default is True.
                 _preload_content (bool): if False, the urllib3.HTTPResponse object
@@ -123,141 +124,6 @@ class QueryApi(object):
                     'version',
                     'query',
                     'dry_run',
-                ],
-                'required': [
-                    'version',
-                    'query',
-                ],
-                'nullable': [
-                ],
-                'enum': [
-                ],
-                'validation': [
-                ]
-            },
-            root_map={
-                'validations': {
-                },
-                'allowed_values': {
-                },
-                'openapi_types': {
-                    'version':
-                        (str,),
-                    'query':
-                        (Query,),
-                    'dry_run':
-                        (bool,),
-                },
-                'attribute_map': {
-                    'version': 'version',
-                    'dry_run': 'dryRun',
-                },
-                'location_map': {
-                    'version': 'path',
-                    'query': 'body',
-                    'dry_run': 'query',
-                },
-                'collection_format_map': {
-                }
-            },
-            headers_map={
-                'accept': [
-                    'application/json'
-                ],
-                'content_type': [
-                    'application/json'
-                ]
-            },
-            api_client=api_client,
-            callable=__boolean_query
-        )
-
-        def __boolean_query2(
-            self,
-            query,
-            version="v3",
-            **kwargs
-        ):
-            """Execute boolean query  # noqa: E501
-
-            Execute a query composed of conditions on columns combined with boolean operators. The generated SQL query is returned in the response.   # noqa: E501
-            This method makes a synchronous HTTP request by default. To make an
-            asynchronous HTTP request, please pass async_req=True
-
-            >>> thread = api.boolean_query2(query, version="v3", async_req=True)
-            >>> result = thread.get()
-
-            Args:
-                query (Query): The boolean query
-                version (str): Dataset version. defaults to "v3", must be one of ["v3"]
-
-            Keyword Args:
-                dry_run (bool): If true, don't run the query, only generate and return it.. [optional] if omitted the server will use the default value of False
-                table (str): table name. [optional] if omitted the server will use the default value of "gdc-bq-sample.cda_mvp"
-                _return_http_data_only (bool): response data without head status
-                    code and headers. Default is True.
-                _preload_content (bool): if False, the urllib3.HTTPResponse object
-                    will be returned without reading/decoding response data.
-                    Default is True.
-                _request_timeout (float/tuple): timeout setting for this request. If one
-                    number provided, it will be total request timeout. It can also
-                    be a pair (tuple) of (connection, read) timeouts.
-                    Default is None.
-                _check_input_type (bool): specifies if type checking
-                    should be done one the data sent to the server.
-                    Default is True.
-                _check_return_type (bool): specifies if type checking
-                    should be done one the data received from the server.
-                    Default is True.
-                _host_index (int/None): specifies the index of the server
-                    that we want to use.
-                    Default is read from the configuration.
-                async_req (bool): execute request asynchronously
-
-            Returns:
-                QueryCreatedData
-                    If the method is called asynchronously, returns the request
-                    thread.
-            """
-            kwargs['async_req'] = kwargs.get(
-                'async_req', False
-            )
-            kwargs['_return_http_data_only'] = kwargs.get(
-                '_return_http_data_only', True
-            )
-            kwargs['_preload_content'] = kwargs.get(
-                '_preload_content', True
-            )
-            kwargs['_request_timeout'] = kwargs.get(
-                '_request_timeout', None
-            )
-            kwargs['_check_input_type'] = kwargs.get(
-                '_check_input_type', True
-            )
-            kwargs['_check_return_type'] = kwargs.get(
-                '_check_return_type', True
-            )
-            kwargs['_host_index'] = kwargs.get('_host_index')
-            kwargs['version'] = \
-                version
-            kwargs['query'] = \
-                query
-            return self.call_with_http_info(**kwargs)
-
-        self.boolean_query2 = _Endpoint(
-            settings={
-                'response_type': (QueryCreatedData,),
-                'auth': [],
-                'endpoint_path': '/api/v1/boolean-query2/{version}',
-                'operation_id': 'boolean_query2',
-                'http_method': 'POST',
-                'servers': None,
-            },
-            params_map={
-                'all': [
-                    'version',
-                    'query',
-                    'dry_run',
                     'table',
                 ],
                 'required': [
@@ -309,7 +175,7 @@ class QueryApi(object):
                 ]
             },
             api_client=api_client,
-            callable=__boolean_query2
+            callable=__boolean_query
         )
 
         def __bulk_data(
--- docs/QueryApi.md
+++ docs/QueryApi.md
@@ -5,7 +5,6 @@ All URIs are relative to *https://cda.cda-dev.broadinstitute.org*
 Method | HTTP request | Description
 ------------- | ------------- | -------------
 [**boolean_query**](QueryApi.md#boolean_query) | **POST** /api/v1/boolean-query/{version} | Execute boolean query
-[**boolean_query2**](QueryApi.md#boolean_query2) | **POST** /api/v1/boolean-query2/{version} | Execute boolean query
 [**bulk_data**](QueryApi.md#bulk_data) | **GET** /api/v1/bulk-data/{version} | Return all data in CDA
 [**query**](QueryApi.md#query) | **GET** /api/v1/query/{id} | Given a query ID, return the a page of data from the query result.
 [**sql_query**](QueryApi.md#sql_query) | **POST** /api/v1/sql-query | Execute SQL directly on a version of the dataset
@@ -46,6 +45,7 @@ with cda_client.ApiClient() as api_client:
         r=Query(),
     ) # Query | The boolean query
     dry_run = False # bool | If true, don't run the query, only generate and return it. (optional) if omitted the server will use the default value of False
+    table = "gdc-bq-sample.cda_mvp" # str | table name (optional) if omitted the server will use the default value of "gdc-bq-sample.cda_mvp"
 
     # example passing only required values which don't have defaults set
     try:
@@ -59,94 +59,10 @@ with cda_client.ApiClient() as api_client:
     # and optional values
     try:
         # Execute boolean query
-        api_response = api_instance.boolean_query(query, dry_run=dry_run)
+        api_response = api_instance.boolean_query(query, dry_run=dry_run, table=table)
         pprint(api_response)
     except cda_client.ApiException as e:
         print("Exception when calling QueryApi->boolean_query: %s\n" % e)
-```
-
-
-### Parameters
-
-Name | Type | Description  | Notes
-------------- | ------------- | ------------- | -------------
- **query** | [**Query**](Query.md)| The boolean query |
- **version** | **str**| Dataset version | defaults to "v3"
- **dry_run** | **bool**| If true, don&#39;t run the query, only generate and return it. | [optional] if omitted the server will use the default value of False
-
-### Return type
-
-[**QueryCreatedData**](QueryCreatedData.md)
-
-### Authorization
-
-No authorization required
-
-### HTTP request headers
-
- - **Content-Type**: application/json
- - **Accept**: application/json
-
-
-### HTTP response details
-| Status code | Description | Response headers |
-|-------------|-------------|------------------|
-**200** | query created response |  -  |
-
-[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
-
-# **boolean_query2**
-> QueryCreatedData boolean_query2(query)
-
-Execute boolean query
-
-Execute a query composed of conditions on columns combined with boolean operators. The generated SQL query is returned in the response. 
-
-### Example
-
-```python
-import time
-import cda_client
-from cda_client.api import query_api
-from cda_client.model.query import Query
-from cda_client.model.query_created_data import QueryCreatedData
-from pprint import pprint
-# Defining the host is optional and defaults to https://cda.cda-dev.broadinstitute.org
-# See configuration.py for a list of all supported configuration parameters.
-configuration = cda_client.Configuration(
-    host = "https://cda.cda-dev.broadinstitute.org"
-)
-
-
-# Enter a context with an instance of the API client
-with cda_client.ApiClient() as api_client:
-    # Create an instance of the API class
-    api_instance = query_api.QueryApi(api_client)
-    query = Query(
-        node_type="column",
-        value="value_example",
-        l=Query(),
-        r=Query(),
-    ) # Query | The boolean query
-    dry_run = False # bool | If true, don't run the query, only generate and return it. (optional) if omitted the server will use the default value of False
-    table = "gdc-bq-sample.cda_mvp" # str | table name (optional) if omitted the server will use the default value of "gdc-bq-sample.cda_mvp"
-
-    # example passing only required values which don't have defaults set
-    try:
-        # Execute boolean query
-        api_response = api_instance.boolean_query2(query)
-        pprint(api_response)
-    except cda_client.ApiException as e:
-        print("Exception when calling QueryApi->boolean_query2: %s\n" % e)
-
-    # example passing only required values which don't have defaults set
-    # and optional values
-    try:
-        # Execute boolean query
-        api_response = api_instance.boolean_query2(query, dry_run=dry_run, table=table)
-        pprint(api_response)
-    except cda_client.ApiException as e:
-        print("Exception when calling QueryApi->boolean_query2: %s\n" % e)
 ```
 
 

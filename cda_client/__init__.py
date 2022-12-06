d3f9fc0288fb0814522106a530047f8d787e9f07
rfricke-asymmetrik <rfricke@asymmetrik.com>
2022-12-06 11:18:53 -0500

    Upgraded swagger versions

--- cda_client/__init__.py
+++ cda_client/__init__.py
@@ -10,7 +10,7 @@
 """
 
 
-__version__ = "9.22.2022"
+__version__ = "12.6.2022"
 
 # import ApiClient
 from cda_client.api_client import ApiClient
--- cda_client/configuration.py
+++ cda_client/configuration.py
@@ -380,7 +380,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 3.0.0\n"\
-               "SDK Package Version: 9.22.2022".\
+               "SDK Package Version: 12.6.2022".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):

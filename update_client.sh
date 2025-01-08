cdde7c05b2f450a47756b9993638ceaafb30c387
tanner-coon-bh <tanner.coon@bluehalo.com>
2025-01-08 12:39:46 -0700

    Adding command to ensure openapi-python-client is installed

--- update_client.sh
+++ update_client.sh
@@ -1,3 +1,4 @@
+pip3 install openapi-python-client
 source .venv/bin/activate
 cd ../
 openapi-python-client generate --url http://127.0.0.1:8000/openapi.json --config cda-service-python-client/openapi_config.yml --overwrite
